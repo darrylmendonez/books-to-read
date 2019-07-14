1dd6810385b95e1bf4b425373b231029759e041e
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-07-14 09:12:56 -0400

    Updates

---
+++ precache-manifest.ccf91699ffd8a24b16a6c2dc44eee5f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9baba29346ed0818256ddeb27e8c8203",
+    "url": "/books-to-read/index.html"
+  },
+  {
+    "revision": "f2d1d9828a4ac6017d35",
+    "url": "/books-to-read/static/css/main.b5fef3b3.chunk.css"
+  },
+  {
+    "revision": "9445687158d5b88835e3",
+    "url": "/books-to-read/static/js/2.44afff6c.chunk.js"
+  },
+  {
+    "revision": "f2d1d9828a4ac6017d35",
+    "url": "/books-to-read/static/js/main.692b4ef8.chunk.js"
+  },
+  {
+    "revision": "27ca7233853e9b731050",
+    "url": "/books-to-read/static/js/runtime~main.bc730f6c.js"
+  }
+]);
